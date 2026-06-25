56e00f54c7214eaf097c6395086c1cbd0ce7d549
Kanav Singla <singla.kanav.work.2808@gmail.com>
2026-06-25 10:10:38 +0530

    fix: add unsafe-inline to CSP script-src to fix blank page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://pdftalk.kanavsingla.f
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src 'self' blob: data:;
   font-src 'self';
